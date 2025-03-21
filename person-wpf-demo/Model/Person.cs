3b29a4d52c309ac054771c52f11d3436b781496a
exeloz <christophercoulombe@gmail.com>
2025-03-21 18:03:47 -0400

    Model Person : Ajout de la Date de naissance

--- person-wpf-demo/Model/Person.cs
+++ person-wpf-demo/Model/Person.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Prenom { get; set; }
         public string Nom { get; set; }
-
+        public DateTime DateDeNaissance { get; set; }
         public List<Address> Addresses { get; set; }
     }
 }
